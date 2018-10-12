4d272d891795f4cef33fdc3b1a556bb60f612afc
Dinith Wannigama <wannigamadinith@gmail.com>
2018-10-12 18:55:21 +1300

    finished q1

--- src/se254/a4/q1/Counter.java
+++ src/se254/a4/q1/Counter.java
@@ -2,8 +2,10 @@ package se254.a4.q1;
 //@formatter:off
 public class Counter {
 	public int c;
-	public String dog = "woof";
+	public static String dog = "woof";
+	public static boolean mouse = true;
 	private int cat = 5;
+	private static double bunny = 67.0;
 
 	public void increment() { c++; }
 
@@ -11,8 +13,21 @@ public class Counter {
 
 	public void reset() { c = 0; }
 	
-	private void donkey() { }
+	public static void a() { dog += "f"; }
 	
-	public void aaaaaaaaaa(String s) { }
+	public void b() { mouse = !mouse; }
+	
+	public static void c(String s) {}
+	
+	private void d() { mouse = !mouse; }
+	
+	private static void e() {}
+	
+	private void f(String s) {}
+	
+	private static void g(String s) {}
+	
+
+
 }
 //@formatter:on
--- src/se254/a4/q1/Q1Main.java
+++ src/se254/a4/q1/Q1Main.java
@@ -3,37 +3,44 @@ package se254.a4.q1;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
  * SE254 Assignment 4 Question 1 Main. This class should be implemented to contain the functionality described in the
  * assignment handout. you may implement additional classes if you wish, but this class should be the main program entry
  * point.
+ * 
+ * 
+ * Completed by: Dinith Wannigama - dwan609 - 834713594
  */
 public class Q1Main {
+	// Keeps track of declared public method of the user-specified class
+	private static List<String> methodList = new ArrayList<String>();
 
 	public static void main(String[] args) {
-		System.out.print("Enter a class name: ");
+		System.out.print("Enter a fully qualified class name: ");
 		String usrIn = getUserInput();
 
 		Class<?> cl = getSpecifiedClass(usrIn);
-		checkValidClass(cl);
+		checkValidObject(cl, 1);
 		Object obj = getSpecifiedObject(cl);
-		System.out.println("You entered: " + usrIn + "\nAn object has been created for this class\n");
+		checkValidObject(obj, 2);
+		System.out.println("An object has been created for this class.\n");
 
 		while (true) {
 			printPublicFields(cl, obj);
 			printPublicMethods(cl);
-			
 			int errCode = 1;
+			// Ask user to enter a method name until a valid one is entered
 			while (errCode != 0) {
-				System.out.println("Enter the name of a method to execute on the object (without the brackets): ");
+				System.out.println("Enter the name of a method to execute on the object (exactly as spelled above and without the brackets): ");
 				String methodName = getUserInput();
 				errCode = performSpecifiedMethod(cl, obj, methodName);
 			}
-
 		}
-
 
 	}
 
@@ -41,7 +48,7 @@ public class Q1Main {
 	private static String getUserInput() {
 		Scanner scan = new Scanner(System.in);
 		String s = scan.next();
-		return s;
+		return s.trim();
 	}
 
 
@@ -55,23 +62,34 @@ public class Q1Main {
 	}
 
 
-	private static void checkValidClass(Class<?> cl) {
-		if (cl == null) {
-			System.err.println("ERROR: Invalid class name... Exiting program...\n\n");
-			System.exit(-1);
-		}
-		return;
-	}
-
-
 	private static Object getSpecifiedObject(Class<?> cl) {
 		try {
 			Object obj = cl.newInstance();
 			return obj;
 		} catch (InstantiationException | IllegalAccessException e) {
-			e.printStackTrace();
 			return null;
 		}
+	}
+	
+	
+	/**
+	 * Checks if the Class or Object made is null or not and prints an error message depending on whether a
+	 * Class or Object is being checked for invalidity. type = 1 is for Class, type = 2 is for Object.
+	 */
+	private static void checkValidObject(Object obj, int type) {
+		if (obj == null) {
+			System.err.println("ERROR: " + checkIfObjectOrClass(type) + "... Exiting program...\n\n");
+			System.exit(-1);
+		}
+		return;
+	}
+	
+	
+	private static String checkIfObjectOrClass(int type) {
+		if (type == 1) {
+			return "Invalid class name";
+		}
+		return "Cannot instantiate this class";
 	}
 
 
@@ -89,10 +107,12 @@ public class Q1Main {
 
 
 	private static void printPublicMethods(Class<?> cl) {
-		System.out.println("Public methods without input parameters:");
-		for (Method m : cl.getMethods()) {
-			if (m.getParameterTypes().length == 0) {
+		methodList.clear();
+		System.out.println("Declared public methods without input parameters:");
+		for (Method m : cl.getDeclaredMethods()) {
+			if (m.getParameterTypes().length == 0 && Modifier.isPublic(m.getModifiers())) {
 				System.out.println("    " + m.getName() + "()");
+				methodList.add(m.getName());
 			}
 		}
 		System.out.println();
@@ -101,15 +121,19 @@ public class Q1Main {
 
 	private static int performSpecifiedMethod(Class<?> cl, Object obj, String methodName) {
 		try {
-			Method m = cl.getMethod(methodName, null);
-			m.invoke(obj, null);
-			System.out.println("\nTHE " + methodName + "() METHOD HAS BEEN INVOKED");
-			System.out.println("=======================================\n");
-			return 0;
+			if (methodList.contains(methodName)) {
+				Method m = cl.getMethod(methodName, null);
+				m.invoke(obj, null);
+				System.out.println("\nTHE " + methodName + "() METHOD HAS BEEN INVOKED");
+				System.out.println("=======================================\n");
+				return 0;
+			} else {
+				throw new Exception("method not declared");
+			}
 		} catch (Exception e) {
-			System.err.println("ERROR: Invalid method name...");
+			System.err.println("ERROR: '" + methodName + "' is and invalid method name. Please select from the list printed above.");
 			return 1;
 		}
 	}
-
+	
 } 
